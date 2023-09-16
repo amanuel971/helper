770ecce629798f6d23bd323127482401d8559f4b
amanuel971 <workuamanuel971@gmail.com>
2023-09-16 12:15:04 -0700

    i low ro commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:HELPER/firstScreen.dart';
 import 'package:HELPER/welcome.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(new MaterialApp(
     home: Welcome(),
   ));
 }
@@ -57,92 +57,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-// import 'package:flutter/material.dart';
-// import 'package:helper/firstScreen.dart';
-// import 'package:helper/HomeScreen.dart';
-
-// //import 'package:helper/Symptom.dart';
-// import 'package:helper/welcome.dart';
-// import 'package:helper/MarkDownPage.dart';
-
-// void main() {
-//   runApp(MaterialApp(
-//     home: Welcome(),
-//   ));
-// }
-
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       appBar: AppBar(
-//           backgroundColor: Colors.blueGrey,
-//           title: Text('Home Page'),
-//           leading: IconButton(
-//             onPressed: () {
-//               Navigator.pop(context);
-//             },
-//             icon: Icon(Icons.arrow_back_ios),
-//           )),
-//       body: Column(
-//         children: [
-//           SizedBox(
-//             height: 20,
-//           ),
-//           Text(
-//             '',
-//             style: TextStyle(fontSize: 20),
-//           ),
-//           SizedBox(height: 80),
-//           Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             children: [
-//               Column(
-//                 children: [
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       Navigator.of(context).push(
-//                         MaterialPageRoute(builder: (context) => firstScreen()),
-//                       );
-//                     },
-//                     child: Text('የአደጋ ጊዜ ጥሪዎች(Emergency Time Calls)'),
-//                   ),
-//                 ],
-//               ),
-//               Column(
-//                 children: [
-//                   ElevatedButton(
-//                     onPressed: () {
-                      // Navigator.push(context, MaterialPageRoute(
-                      //   builder: (context) {
-                      //     return const MarkDownPage(
-                      //       mdName: 'symptom.md',
-                      //       title: 'ምልክቶች',
-                      //     );
-                      //   },
-                      // ));
-//                     },
-//                     child: Text('ምልክቶች(Symptoms)'),
-//                   ),
-//                 ],
-//               ),
-//               Column(
-//                 children: [
-//                   ElevatedButton(
-//                     onPressed: () {
-//                       Navigator.of(context).push(
-//                         MaterialPageRoute(builder: (context) => HomeScreen()),
-//                       );
-//                     },
-//                     child: Text('የመረጃ መመሪያ(Information Guide)'),
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
